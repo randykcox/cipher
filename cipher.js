5addf47544803098ef590f623946c975cec643bd
Randy Cox <randy@kenzie.academy>
2020-12-17 12:44:28 -0500

    Adds caesar shift cipher

--- cipher.js
+++ cipher.js
@@ -1,10 +1,12 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 function transformText (text, textAlphabet, transformAlphabet ) {
 
     let transformedText = ""
     for (let i=0; i<text.length; i++) {
         let index = textAlphabet.indexOf(text[i].toLowerCase())
         if (index < 0) {
-            // not a letter
+            // not a letter, so pass through unmodified
             transformedText += text[i]
         } else {
             transformedText += transformAlphabet[index]
@@ -14,15 +16,32 @@ function transformText (text, textAlphabet, transformAlphabet ) {
     return transformedText
 }
 
-function monoEncipher (plaintext) {
+function stringShift (inputString, shiftAmount) {
+    if (shiftAmount < 0) {
+        throw new Error("Cannot shift backwards (yet)")
+    }
+    let outputArr = inputString.split("")
+    for (let i=0; i<shiftAmount; i++) {
+        let letter = outputArr.shift()
+        outputArr.push(letter)
+    }
+    return outputArr.join("")
+}
 
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    const cipher = "bcdefghijklmnopqrstuvwxyza"
+function monoEncipher (plaintext, cipher = stringShift(alphabet, 1)) {
     return transformText( plaintext, alphabet, cipher )
 }
 
-function monoDecipher (ciphertext) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    const cipher = "bcdefghijklmnopqrstuvwxyza"
+function monoDecipher (ciphertext, cipher = stringShift(alphabet, 1)) {
     return transformText( ciphertext, cipher, alphabet )
+}
+
+function caesarEncipher (plaintext, shiftAmount) {
+    let cipher = stringShift( alphabet, shiftAmount )
+    return monoEncipher( plaintext, cipher )
+}
+
+function caesarDecipher (ciphertext, shiftAmount) {
+    let cipher = stringShift( alphabet, shiftAmount )
+    return monoDecipher( ciphertext, cipher )
 }
